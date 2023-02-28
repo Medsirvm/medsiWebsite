69745db461953f8c1bad8ab0512475d8255244a3
HRios <hrios.isc@gmail.com>
2023-02-28 13:53:02 -0800

    Contracts

--- src/pages/Contract/index.js
+++ src/pages/Contract/index.js
@@ -3,16 +3,13 @@ import {
   CardContent,
   Collapse,
   IconButton,
-  Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import { Container } from "@mui/system";
 import CenteredContent from "../../components/CenteredContent";
 import checkboxNotChecked from "../../assets/icons/checkboxNotChecked.svg";
 import checkboxChecked from "../../assets/icons/checkedCheckbox.svg";
-import { contratPageStyles } from "./ContractPage.styles";
 import CanvaContainer from "./CanvaContainer";
 import Layout from "../../Layouts";
 import { useSelector } from "react-redux";
@@ -20,14 +17,30 @@ import { selectuserInformation } from "../../store/reducers/user/UserAccountSlic
 import ui from './index.module.css';
 import Parraf from '../../components/Parraf';
 import LineBreak from '../../components/LineBreak';
-
 import ContainerTitle from '../../components/ContainerTitle';
+import ContractFile from "./ContractFile";
+
+const ModalContractFancyBox = ({ fancyOpen, handleFancyOpen }) => {
+
+  const { modalFancyBoxContract, fancyBoxContractContainer } = ui;
+
+  return fancyOpen ? (
+    <div style={{ display: "block" }} className={modalFancyBoxContract}>
+      <span className="closeBackground" onClick={() => handleFancyOpen()}></span>
+      <div className={fancyBoxContractContainer}>
+        <ContractFile />
+      </div>
+    </div>
+  ) : null;
+}
 
 const Contract = () => {
-  const classes = contratPageStyles();
   const [open, setOpen] = useState(false);
   const [checkContractOption, setCheckContractOption] = useState(false);
   const userInformation = useSelector(selectuserInformation);
+  const [modalContract, setModalContract] = useState(false);
+  const [fancyOpen, setFancyOpen] = useState(false);
+
   const handleOpenContractCanva = () => {
     setCheckContractOption(!checkContractOption);
     setOpen(!open);
@@ -55,6 +68,8 @@ const Contract = () => {
     cardHeader,
     headerCheckbox,
     headerTitle,
+    contractBox,
+    contractTerms
   } = ui;
 
   const CardHeader = () => {
@@ -70,6 +85,18 @@ const Contract = () => {
         </IconButton>
       </div>
     )
+  }
+
+  useEffect(() => { console.log({ modalContract, fancyOpen }) }, [modalContract, fancyOpen])
+
+  const handleClickContract = () => {
+    setModalContract(!modalContract);
+    handleFancyBox();
+    setFancyOpen(true);
+  }
+
+  const handleFancyBox = () => {
+    setFancyOpen(false);
   }
 
   return (
@@ -90,7 +117,19 @@ const Contract = () => {
                       contrato, así como las condiciones de pago y comisiones e
                       intereses establecidos.{" "}
                     </Parraf>
-                    <LineBreak space={2}/>
+                    <LineBreak space={1} />
+                    <div>
+                      <div className={contractBox} onClick={() => { handleClickContract() }}>
+                        <i className="material-icons-outlined">{modalContract ? "check_box" : "check_box_outline_blank"}</i>
+                        <p className={contractTerms}>Acepto los <strong>Términos y Condiciones de Tanda Ahorros</strong></p>
+                      </div>
+                      <LineBreak space={.5} />
+                      <div className={contractBox} onClick={() => { handleClickContract() }}>
+                        <i className="material-icons-outlined">{modalContract ? "check_box" : "check_box_outline_blank"}</i>
+                        <p className={contractTerms}>Acepto los <strong>Términos y Condiciones de solicitud de crédito</strong></p>
+                      </div>
+                    </div>
+                    <LineBreak space={1} />
                     <CenteredContent direction="column">
                       <CanvaContainer userInformation={userInformation} />
                     </CenteredContent>
@@ -100,6 +139,7 @@ const Contract = () => {
             </div>
           </Card>
         </div>
+        <ModalContractFancyBox fancyOpen={fancyOpen} handleFancyOpen={() => { handleFancyBox() }} />
       </section>
     </Layout>
   );
